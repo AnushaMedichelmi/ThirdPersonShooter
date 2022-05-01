92a6b90e0ca07e528e775d2e58a9509b4b8c8223
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-01 13:06:00 +0530

    SpawnManager script is added.

---
+++ Assets/Scripts/EnemySpawning.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySpawning : MonoBehaviour
+{
+    public static EnemySpawning instance;
+    public GameObject enemyPrefab;
+
+
+    public List<GameObject> Enemy = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        return;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        AddToPool();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void AddToPool()
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+
+            GameObject temp = Instantiate(enemyPrefab);
+
+            Enemy.Add(temp);
+            temp.SetActive(false);
+
+        }
+    }
+
+
+
+    public GameObject EnemiesFromPool()
+    {
+        for (int i = 0; i < Enemy.Count; i++)
+        {
+            if (!Enemy[i].gameObject.activeInHierarchy)
+            {
+                Debug.Log("active");
+                return Enemy[i].gameObject;
+
+            }
+        }
+        return null;
+
+    }
+
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject temp = EnemySpawning.instance.EnemiesFromPool();
+        //Debug.Log(temp.name);
+        if (temp != null)
+        {
+            Debug.Log("true");
+            int k = Random.Range(0, 100);
+
+            if (k < 1)
+            {
+
+                temp.transform.position = temp.transform.position + new Vector3(Random.Range(20f, -45f), 1f, Random.Range(-6f, -15f));
+                temp.SetActive(true);
+                Debug.Log("active");
+            }
+        }
+
+    }
+}
