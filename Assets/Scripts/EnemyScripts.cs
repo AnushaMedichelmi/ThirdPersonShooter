18ce08ec85d7d2b7a39fd0119e8da50159ac30e8
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-01 11:48:23 +0530

    PlayerMovement is done

---
+++ Assets/Scripts/EnemyScripts.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScripts : MonoBehaviour
+{
+    [SerializeField] int startingHealth;            //Declaring the health
+    [SerializeField] int currentHealth;             //Declaring current health of the enemy
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth;         //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DamageMethod(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        Debug.Log("In damage method");
+        if (currentHealth <= 0)
+        {
+            DeathMethod();
+            Debug.Log("Health :" + currentHealth);
+        }
+        Debug.Log("Health :" + currentHealth);
+    }
+
+    private void DeathMethod()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float playerSpeed;
+    CharacterController characterController;
+    Animator animator;
+    public float rotateSpeed;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();      //To get the component
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal") * playerSpeed;        //Movement of the player in horizontal direction
+        float inputZ = Input.GetAxis("Vertical") * playerSpeed;
+        Vector3 movement = new Vector3(inputX, 0f, inputZ);
+        //characterController.SimpleMove(movement*Time.deltaTime);
+        animator.SetFloat("Speed", movement.magnitude);                //To make player from idle to run
+        /* if (movement.magnitude > 0f)
+         {
+             Quaternion tempDirection = Quaternion.LookRotation(movement);
+             transform.rotation = Quaternion.Slerp(transform.rotation, tempDirection, Time.deltaTime * rotateSpeed);
+         }*/
+        transform.Rotate(Vector3.up, inputX * rotateSpeed * Time.deltaTime);      //Rotation of the player
+        if (inputZ != 0)
+        {
+            characterController.SimpleMove(transform.forward * Time.deltaTime * inputZ);    //Movement of the player using Simple move 
+        }
+    }
+}
