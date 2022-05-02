de6b3e8c4901a1a3aafc479c1ac75a4ab4c7d1f4
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-02 11:30:38 +0530

    Audio Source is added.

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -15,7 +15,7 @@ public class GunScript : MonoBehaviour
     public Transform firePoint;
     [SerializeField]
     ParticleSystem particleSystem;      
-    //public AudioSource audio;
+    public AudioSource audio;
     void Start()
     {
 
@@ -30,7 +30,7 @@ public class GunScript : MonoBehaviour
             if (Input.GetButton("Fire1"))
             {
                 
-               // audio.Play();
+                audio.Play();
                 timer = 0f;
                 ToFireGun();
             }
