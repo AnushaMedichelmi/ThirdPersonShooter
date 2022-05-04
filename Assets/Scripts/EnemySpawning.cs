f061716c48b5c62cac26a22d98559c6566e38627
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-04 16:14:12 +0530

    no message

--- Assets/Scripts/EnemySpawning.cs
+++ Assets/Scripts/EnemySpawning.cs
@@ -7,10 +7,11 @@ public class EnemySpawning : MonoBehaviour
 {
     public static EnemySpawning instance;
     public GameObject enemyPrefab;
+   // public GameObject enemyRagDoll;
 
 
     public List<GameObject> Enemy = new List<GameObject>();
-
+  // public List<GameObject> EnemyRagdoll = new List<GameObject>();
     private void Awake()
     {
         if (instance == null)
@@ -46,6 +47,21 @@ public class EnemySpawning : MonoBehaviour
 
         }
     }
+    /*
+    public void AddToRagDollPool()
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+
+            GameObject temp = Instantiate(enemyRagDoll);
+
+            EnemyRagdoll.Add(temp);
+            temp.SetActive(false);
+
+        }
+    }*/
+
 
 
 
@@ -63,5 +79,21 @@ public class EnemySpawning : MonoBehaviour
         return null;
 
     }
+    /*
+
+    public GameObject EnemiesRagDollFromPool()
+    {
+        for (int i = 0; i < Enemy.Count; i++)
+        {
+            if (!EnemyRagdoll[i].gameObject.activeInHierarchy)
+            {
+                Debug.Log("active");
+                return EnemyRagdoll[i].gameObject;
+
+            }
+        }
+        return null;
+
+    }*/
 
 }
